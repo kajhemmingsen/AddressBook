8bd2928345aae76b6002d6d26c41d53b360e2b6b
kajhemmingsen <kajhemmingsen85@gmail.com>
2020-02-07 15:51:14 -0500

    Tests work locally

--- src/test/java/RegControllerTest.java
+++ src/test/java/RegControllerTest.java
@@ -1,6 +1,6 @@
 import AddressBook.RegController;
 import AddressBook.ServingWebContentApplication;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,11 +8,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(classes = {ServingWebContentApplication.class})
 public class RegControllerTest {
 
-   /* @Autowired
+    @Autowired
     private RegController controller;
 
     @Test
     public void contexLoad() throws Exception {
         assertThat(controller).isNotNull();
-    }*/
+    }
 }
